3d7a26e7f06c507d1933a3cfedf5cb1648e35bca
Bellephanthira <86554130+Bellephanthira@users.noreply.github.com>
2021-06-27 18:24:54 +0700

    Add files via upload

---
+++ Exercise 4.py
@@ -0,0 +1,9 @@
+FE = 100
+GB = 95
+ICS = 100
+CP = 85
+print("---Your score---")
+print("Foundation English",FE)
+print("General business", GB)
+print("Introduction to computer systems", ICS)
+print("Computer programming",CP)
